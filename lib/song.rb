938485c9c0b67956465e553ca3a27639dfb2d786
Shamel Lakin <christlife4u@gmail.com>
2019-10-11 12:59:37 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,41 @@
+require "pry"
+class Song 
+  
+  attr_accessor :artist, :name 
+    
+    @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+
+ 
+  def self.all 
+    @@all 
+  end  
+  
+  def artist=(artist)
+      @artist = artist 
+      artist.songs << self
+  end
+   
+    # def artist_name=(name)
+    #   self.artist = Artist.find_or_create_by_name(name)
+    #   self.artist.add_song(self) 
+    # end 
+   
+  def self.new_by_filename(file_name)
+      songs = file_name.split(" _ ")[1]
+      artist = file_name.split(" _ ")[0]
+      songs = self.new(file_name)
+      songs.artist = artist
+      songs
+  end 
+  
+  def artist_name=(name)
+  self.artist= Artist.find_or_create_by_name(name)
+  self.artist.add_song(self) 
+  end 
+  
+end 
