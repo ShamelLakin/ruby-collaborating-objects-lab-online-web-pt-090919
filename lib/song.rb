ed049018f976834520f658c5024f03b25595f52a
Shamel Lakin <christlife4u@gmail.com>
2019-10-11 13:03:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,7 +22,7 @@ class Song
    
    
   def self.new_by_filename(file_name)
-      songs = self.new(file_name)
+      songs = Song.new(file_name)
       songs = file_name.split(" _ ")[1]
       artist = file_name.split(" _ ")[0]
       # songs = self.new(file_name)
